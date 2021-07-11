6b1e6c56b9d6bc65900ec5a5d46791e23dbfc429
Roddy Bae <bsy17171@naver.com>
2021-07-12 01:01:31 +0900

    fgsdf

--- magic.js
+++ magic.js
@@ -1,6 +1,9 @@
 
 const thList = [];
 const tdList = [];
+
+let prayList;
+let penaltyList;
 function createEl(tagType,property,value,classNames,where){
     const e =document.createElement(`${tagType}`); 
     if(property==='innerHTML'){
@@ -21,44 +24,83 @@ function createEl(tagType,property,value,classNames,where){
 const table = document.querySelector("table")
 const thead =document.querySelector(".info");
 const tbody = document.querySelector(".active");
-fetch('./convert.json')
-  .then(response => response.json())
-  .then(async(data) => {
-      for(const key in data.fee){
-          thList.push(key);
-          tdList.push(data.fee[key]);
-        }})
-    
-  .catch(error => console.log(error));
-  
-  console.log(thList,tdList);
-  setTimeout(() => {
-    thList.map((index,data)=>{
-        const th = createEl('th',' ',` `,['thone'],thead);
-        createEl("p",'innerHTML',`${index}`,[],th);
-    });
-    tdList.map((index,data)=>{
-        if(parseInt(index)==0){
-            const td = createEl('td',' ',` `,['success'],tbody);
-            createEl("p",'innerHTML',`${index}`,[],td);    
-        }
-        else if(parseInt(index)<=1000 &&parseInt(index)>0){
-            const td = createEl('td',' ',` `,['warning'],tbody);
-            createEl("p",'innerHTML',`${index}`,[],td); 
-        }
-        else if(parseInt(index)>=1000){
-            const td = createEl('td',' ',` `,['danger'],tbody);
-            createEl("p",'innerHTML',`${index}`,[],td); 
-        }
-        else{
-            const td = createEl('td',' ',` `,['danger'],tbody);
-            createEl("p",'innerHTML',`${index}`,[],td); 
-        }
+const prayer = document.querySelector(".prayer");
 
+  function loadJSON1(callback) {   
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', `Json/pray.json`, true);
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == "200") {
+        callback(JSON.parse(xobj.responseText));
+      }
+    };
+    xobj.send(null);  
+  }
+
+  loadJSON1(function(json) {
+    const trap = json; // this will log out the json object
+    const two =Object.entries(trap)
+    prayList=Object.entries(two);
+    varPray();
+    
+  });
+
+  function loadJSON2(callback) {   
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', `Json/convert.json`, true);
+    xobj.onreadystatechange = function () {
+      if (xobj.readyState == 4 && xobj.status == "200") {
+        callback(JSON.parse(xobj.responseText));
+      }
+    };
+    xobj.send(null);  
+  }
+  loadJSON2(function(json) {
+    const trap = json; // this will log out the json object
+    const two =Object.entries(trap)
+    penaltyList=Object.entries(two[0][1]);
+    varPk();
+  });
+
+ 
+
+  const varPk = (e) => {
+    console.log(penaltyList);
+    penaltyList.map((e)=>{
+          const th = createEl('th',' ',` `,["thsexy"],thead);
+           createEl("p",'innerHTML',`${e[0]}`,[],th);
+           const td = createEl('th',' ',` `,["thsexy"],tbody);
+           createEl("p",'innerHTML',`${e[1]}`,[],td);
+      });
+
+      
+  }
+
+  const varPray = () => {
+    prayList.map(e=>{
+        const ul = createEl("ul",' ','',["list-group"],prayer);
+        createEl("li","innerHTML",`${e[1][0]}`,["list-group-item","active"],ul)
+        Object.entries(e[1][1]).map((e,idx)=>
+        { 
+          console.log(idx)
+          if(idx!=0 && e[1]!="Nan"){
+            createEl("li","innerHTML",`${e[1]}`,["list-group-item"],ul)
+          }
+        }
+        
+        )
     })
 
+  }
+
+  
     
-  }, 500);
+
+
+
+
 
 
 /*data.fee[key]*/
